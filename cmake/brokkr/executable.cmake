3ad31d84e3bc823e20cdcd85792bd9a9f2e32a17
Emery Goss (AIVAS-v2:Ubuntu-20.04) <m.goss792@gmail.com>
2023-01-11 18:50:20 -0600

    feature(cmake): add function `brokkr_executable`

--- cmake/brokkr/executable.cmake
+++ cmake/brokkr/executable.cmake
@@ -1,6 +1,10 @@
 
 # Create an executable target and populate it with all the specified options.
 #
+# This is a lower-level implementation helper function. Prefer the function
+# `brokkr_executable()` below for creating an executable target and
+# configuring installation and associated tests.
+#
 # This will also create an alias target prefixed with "PACKAGE_NAME::". This
 # matches the name the target will take on when referenced in other
 # packages.
@@ -31,4 +35,73 @@ function(brokkr_add_executable EXEC_NAME)
     target_compile_options(${EXEC_NAME} PRIVATE ${BKR_ADD_EXEC_COMPILE_OPTIONS})
 
     add_executable(${PROJECT_NAME}::${EXEC_NAME} ALIAS ${EXEC_NAME})
+endfunction()
+
+
+# Create an executable and flag it for installation.
+#
+# The executable source is assumed to be a single source file. The expectation
+# is that almost all of the work will be handled by library code which is
+# covered by tests. The single source file for the executable should thus be
+# very small and delegate almost everything to libraries.
+#
+# Dependencies do not have to be `find_package`d separately if they conform to
+# the following:
+#  1) The target name is either local to the project or is qualified by the
+#     package name.
+#  2) The target can be imported with a simple `find_package(package_name
+#     REQUIRED)` call. It is assumed that versions are managed by an external
+#     package manager. (Components are not yet supported.)
+#
+# :param EXEC_NAME: Name of the executable target.
+# :type EXEC_NAME: String. (required)
+# :param NO_INSTALL: Suppress installation of the executable target.
+# :type NO_INSTALL: Flag.
+# :param MAIN_SOURCE: Path to the main source file. Defaults to
+#     "${EXEC_NAME}.cpp".
+# :type MAIN_SOURCE: Path to a file, relative or absolute. (optional)
+# :param DEPENDENCIES: Targets to link the new executable to.
+# :type DEPENDENCIES: List of target names.
+function(brokkr_executable EXEC_NAME)
+    cmake_parse_arguments(
+        PARSE_ARGV 1
+        BKR_EXEC
+        "NO_INSTALL"
+        "MAIN_SOURCE"
+        "DEPENDENCIES"
+    )
+
+    # Set defaults.
+    if(NOT BKR_EXEC_MAIN_SOURCE)
+        set(BKR_EXEC_MAIN_SOURCE "${EXEC_NAME}.cpp")
+    endif()
+
+    # Import dependencies as needed.
+    _bkr_ensure_found(
+        OUTPUT_PACKAGES pkg_dependencies
+        TARGETS ${BKR_EXEC_DEPENDENCIES}
+    )
+
+    # Create executable target.
+    brokkr_add_executable(
+        ${EXEC_NAME}
+        SOURCES "${BKR_EXEC_MAIN_SOURCE}"
+        DEPENDENCIES ${BKR_EXEC_DEPENDENCIES}
+        COMPILE_OPTIONS
+            -Werror
+            -Wall
+            -Wextra
+            -Wpedantic
+    )
+
+    # Static analysis????
+    # TODO
+
+    # Flag for installation.
+    if(NOT BKR_EXEC_NO_INSTALL)
+        brokkr_install_target(
+            ${EXEC_NAME}
+            REQUIRED_PACKAGES ${pkg_dependencies}
+        )
+    endif()
 endfunction()
