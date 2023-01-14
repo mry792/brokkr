77be1976b3c4bb73b278ad36bb52b0b00d096f96
Emery Goss (AIVAS-v2:Ubuntu-20.04) <m.goss792@gmail.com>
2023-01-14 12:20:01 -0600

    feature(cmake): add function `brokkr_add_library_unit_tests`

--- cmake/brokkr/library.cmake
+++ cmake/brokkr/library.cmake
@@ -88,3 +88,80 @@ function(brokkr_add_library LIB_NAME)
 
     add_library(${PROJECT_NAME}::${LIB_NAME} ALIAS ${LIB_NAME})
 endfunction()
+
+
+# Create an executable target for a library's unit tests.
+#
+# This is a lower-level implementation helper function. The function
+# `brokkr_library()` below will call this function to generate the unit test
+# target and register it. Prefer to not call `brokkr_add_library_unit_tests()`
+# directly.
+#
+# :param LIB_NAME: Name of the library under test.
+# :type LIB_NAME: Target name.
+# :param DISCOVER_COMMAND: CMake command used to register the tests with CTest.
+# :type DISCOVER_COMMAND: Command name. (optional)
+# :param SOURCES: Source files to compile into the test executable.
+# :type SOURCES: List of paths to files.
+# :param DEPENDENCIES: Targets to link the test executable to.
+# :type DEPENDENCIES: List of target names.
+# :param DISCOVER_EXTRA_ARGS: Extra arguments to the discover command.
+# :type DISCOVER_EXTRA_ARGS: List of strings.
+#
+# All unparsed arguments will be forwarded to the `brokkr_add_executable`
+# command that creates the unit test executable target.
+function(brokkr_add_library_unit_tests LIB_NAME)
+    if(CMAKE_PROJECT_NAME STREQUAL PROJECT_NAME)
+        include(CTest)
+
+        if(BUILD_TESTING)
+            cmake_parse_arguments(
+                PARSE_ARGV 1
+                BKR_ADD_LIB_UT
+                ""
+                "DISCOVER_COMMAND"
+                "SOURCES;DEPENDENCIES;DISCOVER_EXTRA_ARGS"
+            )
+
+            set(ut_exec_name "${LIB_NAME}_unit-tests")
+
+            if(BKR_ADD_LIB_UT_SOURCES OR BKR_ADD_LIB_UT_DEPENDENCIES)
+                # Generate empty source file if needed.
+                if(NOT BKR_ADD_LIB_UT_SOURCES)
+                    message(
+                        WARNING
+                        "No unit test sources found for unit test executable "
+                        "\"${ut_exec_name}\" of library \"${LIB_NAME}\". "
+                        "Generating unit test executable only from linked "
+                        "dependencies."
+                    )
+                    file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/brokkr_empty.cpp "")
+                    list(
+                        APPEND
+                        BKR_ADD_LIB_UT_SOURCES
+                        ${CMAKE_CURRENT_BINARY_DIR}/brokkr_empty.cpp
+                    )
+                endif()
+
+                _bkr_ensure_found(TARGETS ${BKR_ADD_LIB_UT_DEPENDENCIES})
+                brokkr_add_executable(
+                    "${ut_exec_name}"
+                    SOURCES ${BKR_ADD_LIB_UT_SOURCES}
+                    DEPENDENCIES
+                        ${LIB_NAME}
+                        ${BKR_ADD_LIB_UT_DEPENDENCIES}
+                    ${BKR_ADD_LIB_UT_UNPARSED_ARGUMENTS}
+                )
+
+                if(BKR_ADD_LIB_UT_DISCOVER_COMMAND)
+                    cmake_language(
+                        CALL
+                        ${BKR_ADD_LIB_UT_DISCOVER_COMMAND}
+                        ${ut_exec_name}
+                        ${BKR_ADD_LIB_UT_DISCOVER_EXTRA_ARGS}
+                    )
+                endif()
+            endif()
+        endif()
+    endif()
+endfunction()
